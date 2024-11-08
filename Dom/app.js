b0f0857bb778971f7a7751aa6a23498c51e7c012
Javier Ulices <ulicesjaviermartinez@gmail.com>
2024-11-08 15:34:52 -0600

    Cambios

--- Dom/app.js
+++ Dom/app.js
@@ -27,10 +27,10 @@ li.textContent = 'Primer Elemento';
 // Y lo agregamos a la lista
 lista.appendChild(li);*/
 
-const lista = document.querySelector('#lista');
+//const lista = document.querySelector('#lista');
 // Creando el array
 
-const ArrayElement = ['Primer Elemento', 'Segundo', 'Tercero', 'Este es el cuarto Elemento'];
+//const ArrayElement = ["Primer Elemento", "Segundo", "Tercero", "Cuarto item"];
 // Recorriendo el array y agregando los elementos a la lista
 /*ArrayElement.forEach( item => {
     /*const li = document.createElement('li'); // Creamos un li en la primera iteracion
@@ -68,11 +68,46 @@ lista.appendChild(fragment);*/
 });
 // Agregando el fragmento a la lista
 lista.appendChild(fragment);*/
+/*const lista = document.querySelector('#lista');
+const ArrayElement = ["Primer Elemento", "Segundo", "Tercero", "Cuarto item"];
+const template = document.querySelector('#template-li').content; // Accedemos al contenido del template
+const fragment = document.createDocumentFragment();
 
-let fragment = '';
 ArrayElement.forEach((item) => {
-    fragment += `<li class="list"><b>Nombre : </b><span>${item}</
-    span></li>`
-    });
+    const clone = template.cloneNode(true); // Clonamos el contenido del template
+    clone.querySelector('.text-danger').textContent = item; // Modificamos el contenido del clone
+    fragment.appendChild(clone); // Añadimos el clone al fragmento
+});
+
+lista.appendChild(fragment); // Añadimos el fragmento a la lista
+
+
+
 // Agregando el fragmento a la lista
-    lista.innerHTML = fragment;
+    //lista.innerHTML = fragment;*/
+
+
+    const btnAumentar = document.querySelector(".btn-info");
+    const span = document.getElementById("Span"); // Cambiado a "Span" con mayúscula
+    
+    // Crear contador
+    let contador = 0;
+    
+    // Agregar evento click al botón Aumentar
+    btnAumentar.addEventListener("click", () => {
+        console.log("Me diste clic, así que aumentaré");
+        contador++;
+        span.textContent = contador;
+    });
+
+// Agregar evento click al botón Disminuir
+const btnDisminuir = document.querySelector(".btn-danger");
+btnDisminuir.addEventListener("click", () => {
+    console.log("Me diste clic, así que disminuiré");
+    contador--;
+    if (contador < 0) {
+        contador = 0;
+        }
+        span.textContent = contador;
+        });
+        
